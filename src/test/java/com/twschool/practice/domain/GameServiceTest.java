fdeae78e1d7789db8d3588295aae99a979e51382
yangjie <yangjienice@126.com>
2020-06-22 19:56:15 +0800

    修改gameservice测试

--- src/test/java/com/twschool/practice/domain/GameServiceTest.java
+++ src/test/java/com/twschool/practice/domain/GameServiceTest.java
@@ -3,6 +3,7 @@ package com.twschool.practice.domain;
 import com.twschool.practice.GameService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class GameServiceTest {
     @Test
@@ -11,5 +12,13 @@ public class GameServiceTest {
         GameService gameService = new GameService(guessNumberGame);
         String result = gameService.guess("1 2 3 4");
         Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void should_return_result_when_guess_number(){
+        GuessNumberGame guessNumberGame = Mockito.mock(GuessNumberGame.class);
+        GameService gameService = new GameService(guessNumberGame);
+        gameService.guess("1 2 3 4");
+        Mockito.verify(guessNumberGame,Mockito.times(1)).guess(Mockito.eq("1 2 3 4"));
     }
 }
